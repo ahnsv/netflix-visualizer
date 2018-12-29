1d482ea1e3c01d33fbd9f2b31fbf4f1f55289786
Humphrey Ahn <ahnsv@bc.edu>
2018-12-29 14:41:11 +0900

    feature: webpack+dev server settings added

--- public/dist/server.js
+++ public/dist/server.js
@@ -1,11 +1,1 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var path_1 = __importDefault(require("path"));
-var app = express_1.default();
-app.use(express_1.default.static('public'));
-app.get('/', function (req, res) { return res.sendFile(path_1.default.join(__dirname + '/../index.html')); });
-app.listen(3000, function () { return console.log('Example app listening on port 3000!'); });
+!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){"use strict";(function(e){var r=this&&this.__importDefault||function(e){return e&&e.__esModule?e:{default:e}};Object.defineProperty(t,"__esModule",{value:!0});var o=r(n(1)),u=r(n(2)),i=o.default();i.use(o.default.static("public")),i.get("/",function(t,n){return n.sendFile(u.default.join(e+"/../index.html"))}),i.listen(3e3,function(){return console.log("Example app listening on port 3000!")})}).call(this,"/")},function(e,t){e.exports=require("express")},function(e,t){e.exports=require("path")}]);
